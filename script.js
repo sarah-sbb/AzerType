294283c4bd972df75ff06fa5f9631f56f2a1942a
Sarah BASSET <boha.design@outlook.fr>
2024-05-07 16:47:06 +0200

    feat(test):add js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// let motTapeOk = false
+
+let motUtilisateur = prompt("Entrez un mot :")
+console.log(motUtilisateur)
+
+// if (motTapeOk) {
+//     console.log("Bravo, vous avez correctement écrit le mot")
+// } else {
+//     console.log("Échec, le mot n'est pas correct")
+// }
